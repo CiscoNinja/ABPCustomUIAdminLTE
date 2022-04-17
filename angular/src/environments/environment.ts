123572e7087633d2163a36a2ebcc99baaa307926
Francisco Kwame Kadzi <franciscokadzi@gmail.com>
2022-04-17 00:26:10 +0000

    rebased for angular

---
+++ angular/src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'CustomUIAdminLTE',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44384',
+    redirectUri: baseUrl,
+    clientId: 'CustomUIAdminLTE_App',
+    responseType: 'code',
+    scope: 'offline_access CustomUIAdminLTE',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44384',
+      rootNamespace: 'CustomUIAdminLTE',
+    },
+  },
+} as Environment;
